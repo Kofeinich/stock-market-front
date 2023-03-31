a53202cf1e1c5b6ceafc9d753d290ba91aca177c
Kofeinich <kofeinich1@gmail.com>
2023-03-31 23:11:13 +0300

    feat: check this shit bro

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,15 @@ import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
-import {AuthPage} from "./pages/AuthPage";
 import {UserPage} from "./pages/UserPage";
-import {AdminPage} from "./pages/AdminPage";
+
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <AuthPage/>,
-    },
-    {
-        path: "/user",
         element: <UserPage/>,
     },
-    {
-        path: "/admin",
-        element: <AdminPage/>,
-    }
 ]);
 
 
